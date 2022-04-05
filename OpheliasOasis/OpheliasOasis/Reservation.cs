70af68629e8d42d39e162436a4dee9e3cef12fb8
nathanielgreenna <61034629+nathanielgreenna@users.noreply.github.com>
2022-04-04 23:32:15 -0400

    Add files via upload

---
+++ OpheliasOasis/OpheliasOasis/Reservation.cs
@@ -0,0 +1,102 @@
+﻿/*
+ * Reservation
+ * TODO: Should more or less be done. subject to changes
+ * Changelog:
+ * 4/4/2022: created/getters and setters coded by Nathaniel
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpheliasOasis
+{
+    class Reservation
+    {
+        private String reservationType;
+        private String customerName;
+        private int customerCreditCard;
+        private String customerEmail;
+        private int roomNumber;
+        private ReservationDate startDate;
+        private ReservationDate endDate;
+        private int totalPrice;
+        private String reservationStatus;
+
+        String getReservationType() 
+        {
+            return reservationType;
+        }
+        void setReservationType(String type) 
+        {
+            reservationType = type;
+        }
+        String getCustomerName() 
+        {
+            return customerName;
+        }
+        void setCustomerName(String name) 
+        {
+            customerName = name;
+        }
+        int getCustomerCreditCard()
+        {
+            return customerCreditCard;
+        }
+        void setCustomerCreditCard(int card)
+        {
+            customerCreditCard = card;
+        }
+        String getCustomerEmail() 
+        {
+            return customerEmail;
+        }
+        void setCustomerEmail(String email) 
+        {
+            customerEmail = email;
+        }
+        int getRoomNumber() 
+        {
+            return roomNumber;
+        }
+
+        //don't know if we should be able to set rooms
+
+        ReservationDate getStartDate() 
+        {
+            return startDate;
+        }
+
+        //don't know if we should be able to directly set startDate
+
+        ReservationDate getEndDate()
+        {
+            return endDate;
+        }
+
+        //don't know if we should be able to directly set endDate
+
+        int getTotalPrice() 
+        {
+            return totalPrice;
+        }
+
+        //don't know if we should be able to directly set price
+
+        String getReservationStatus()
+        {
+            return reservationStatus;
+        }
+        void setReservationStatus(String status)
+        {
+            reservationStatus = status;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ OpheliasOasis/OpheliasOasis/ReservationDate.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * ReservationDate
+ * TODO: Should more or less be done. subject to changes
+ * Changelog:
+ * 4/4/2022: created/initially coded by Nathaniel
+*/
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpheliasOasis
+{
+    
+
+    class ReservationDate
+    {
+        private DateTime date;
+        private int basePrice;
+        private int occupancy = 0;
+
+        ReservationDate(DateTime newDate) 
+        {
+            date = newDate;
+        }
+        DateTime getDate() 
+        {
+            return (date);
+        }
+
+
+        int getBasePrice() 
+        {
+            return (basePrice);
+        }
+        void setBasePrice(int newPrice)
+        {
+            basePrice = newPrice;
+        }
+        void increaseOccupancy() 
+        {
+            occupancy++;
+        }
+        void decreaseOccupancy()
+        {
+            occupancy--;
+        }
+        int getOccupancy()
+        {
+            return (occupancy);
+        }
+
+
+    }
+}
