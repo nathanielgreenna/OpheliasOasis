260a92d18ba7011286e43d469b29c4642289c87d
A. Peterson <44214859+IAmAPeterson@users.noreply.github.com>
2022-04-22 15:19:51 -0400

    Update Program.cs
    Added reservationMenu
    Added stubs for the different processes associated with it

--- OpheliasOasis/OpheliasOasis/Program.cs
+++ OpheliasOasis/OpheliasOasis/Program.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿/*
+ * Program
+ * 
+ * Description: The controlling class for the program. Loads the database and allows
+ * the user to manipulate it. See methodss for more details.
+ * 
+ * Changelog:
+ */
+
+using System;
 
 namespace OpheliasOasis
 {
-
-
+    /// <summary>
+    /// The controlling class for Ophelia's Oasis Reservation System
+    /// </summary>
     class Program
     {
+
+        private static ReservationDB reservationDB;
+
         static void Main(string[] args)
         {
             ReservationDB rdb = new ReservationDB();
@@ -27,7 +40,70 @@ namespace OpheliasOasis
             r.setCustomerCreditCard(99);
             Console.WriteLine(rdb2.getReservation(DateTime.Today)[0].getCustomerCreditCard());
 
+            while(true)
+            {
+                System.Threading.Thread.Sleep(2000);
+                ReservationMenu();
+            }
 
+        }
+
+        /// <summary>
+        /// Display the menu for adding, creating, or removing reservations.
+        /// </summary>
+        static void ReservationMenu()
+        {
+            // Display menu options
+            DisplayMenuHeader("Reservation");
+            Console.WriteLine("Please select one of the following options:");
+            Console.WriteLine("\t1: Place a new reservation");
+            Console.WriteLine("\t2: Update an existing Reservation");
+            Console.WriteLine("\t3: Cancel an existing reservation");
+            Console.WriteLine("\t4: Return to the main menu");
+            Console.WriteLine();
+
+            string selectionText;
+            int selection;
+            
+            Console.Write("Please enter your selection (1-4): ");
+            selectionText = Console.ReadLine();
+
+            // If necessary, repeatedly request a selection until a valid number (1-4) is provided
+            while (!int.TryParse(selectionText, out selection) || selection > 4 || selection < 1)
+            {
+                Console.Write("Selection \"" + selectionText + "\" is not valid. Please enter your selection (1-4): ");
+                selectionText = Console.ReadLine();
+            }
+
+            switch (selection)
+            {
+                case 1: PlaceReservation(); break;
+                case 2: UpdateReservation(); break;
+                case 3: CancelReservation(); break;
+                case 4: return;
+            }
+        }
+
+        static void PlaceReservation()
+        {
+            
+        }
+
+        static void UpdateReservation()
+        {
+            Console.WriteLine("Modify");
+        }
+
+        static void CancelReservation()
+        {
+            Console.WriteLine("Cancel");
+        }
+
+        static void DisplayMenuHeader(string menuName)
+        {
+            Console.Clear();
+            Console.WriteLine("====================| " + menuName.ToUpper() + " MENU |====================");
+            Console.WriteLine();
         }
     }
 }
